af21552678fbe929fd2b0e656c8266a7bed64b33
devfardin <contactfardin22@gmail.com>
2025-11-27 20:06:50 +0600

    implement lightbox when click over the image

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     const ul = $('.eael-filter-gallery-control ul');
     const li = $('<li></li>');
     li.attr({
@@ -17,7 +17,7 @@ jQuery(document).ready(function($) {
             const forData = new FormData();
             forData.append('action', 'post_data_ajax');
             forData.append('nonce', filterable_gallery_params.nonce);
-            
+
             fetch(filterable_gallery_params.ajax_url, {
                 method: 'POST',
                 body: forData
@@ -29,22 +29,24 @@ jQuery(document).ready(function($) {
                     masseuseItem.html(`
                         <div class="eael-gallery-grid-item">
                             <div class="gallery-item-thumbnail-wrap">
-                                <img src="${item.thumbnail}" alt="${item.title}" class="gallery-item-thumbnail">
+                                <img src="${item.thumbnail}" alt="${item.title}" class="gallery-item-thumbnail" data-lazy-src="http://alibi-massage.local/wp-content/uploads/2025/11/b162154a5459d85edb24265ae1c819f9.jpg" />
                             </div>
-                            <div class="gallery-item-caption-wrap caption-style-hoverer eael-slide-up">
-                                <div class="gallery-item-hoverer-bg"></div>
-                                <div class="gallery-item-caption-over">
-                                    <h2 class="fg-item-title">${item.title}</h2>
-                                    <div class="fg-item-content">
-                                        <p>${item.content}</p>
+                            <a area-hidden="true" aria-label="eael-magnific-link" href="${item.thumbnail}" class="eael-magnific-link eael-magnific-link-clone active media-content-wrap" data-elementor-open-lightbox="yes" title="">
+                                <div class="gallery-item-caption-wrap caption-style-hoverer eael-slide-up">
+                                    <div class="gallery-item-hoverer-bg"></div>
+                                    <div class="gallery-item-caption-over">
+                                        <h2 class="fg-item-title">${item.title}</h2>
+                                        <div class="fg-item-content">
+                                            <p>Masseuses</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </a>
                         </div>
                     `);
                     items.push(masseuseItem[0]);
                 });
-                
+
                 galleryContainer.append(items);
                 const isotope = galleryContainer.data('isotope');
                 if (isotope) {
